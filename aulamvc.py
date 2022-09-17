fe18e8c084c2931ebef9de355d856741cf9fd9fc
rafaelgac98 <80112965+rafaelgac98@users.noreply.github.com>
2022-09-16 22:27:59 -0300

    Update aulamvc.py

--- aulamvc.py
+++ aulamvc.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '123456'
-app.config['MYSQL_DATABASE_DB'] = 'bd_aluno'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
 app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
