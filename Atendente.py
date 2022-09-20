163d3bc1c212dd3a832f69d0b473058b37ca9404
rafaelgac98 <80112965+rafaelgac98@users.noreply.github.com>
2022-09-19 21:54:54 -0300

    Delete Atendente.py

--- Atendente.py
+++
@@ -1,9 +0,0 @@
-class Atendente(object):
-    def __init__(self, CpfAtendente, NomeAtendente, SobrenomeAtendente, RgAtendente, EnderecoAtendente, SalarioAtendente, TelefoneAtendente):
-        self.CpfAtendente = CpfAtendente
-        self.NomeAtendente = NomeAtendente
-        self.SobrenomeAtendente = SobrenomeAtendente
-        self.RgAtendente = RgAtendente
-        self.EnderecoAtendente = EnderecoAtendente
-        self.SalarioAtendente = SalarioAtendente
-        self.TelefoneAtendente = TelefoneAtendente
