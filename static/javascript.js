8674e6b8af2ea5da0ee6fe52853a7143ed011947
rafaelgac98 <80112965+rafaelgac98@users.noreply.github.com>
2022-09-29 22:08:04 -0300

    Add files via upload

---
+++ static/javascript.js
@@ -0,0 +1,17 @@
+(function(win, doc){
+    'use strict';
+    //Verifica se o usuário deseja mesmo deletar este dado
+    if(doc.querySelector('.btnDel')){
+        let btnDel = doc.querySelectorAll('.btnDel');
+        for(let i=0; i < btnDel.length; i++){
+            btnDel[i].addEventListener('click', function(event ){
+                if(confirm('Deseja mesmo apagar este dado?')){
+                    return true;
+                }else{
+                    event.preventDefault();
+                }
+            });
+        }
+    }
+
+})(window, document);
