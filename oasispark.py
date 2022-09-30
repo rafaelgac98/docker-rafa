f0c15dbdf31e56b36d55850246fd7938bdb08f54
rafaelgac98 <80112965+rafaelgac98@users.noreply.github.com>
2022-09-30 20:59:12 -0300

    Update oasispark.py

--- oasispark.py
+++ oasispark.py
@@ -63,7 +63,7 @@ def veiculo():
 
 
 
-@app.route('/listaparaalteraatendente/<int:pk>/', methods=['GET'])
+@app.route('/listaparaalteraatendente/<int:pk>/', methods=['POST', 'GET'])
 def listaparaalteraatendente(pk):    
     conn1 = mysql.connect()
     cursor1 = conn1.cursor()
@@ -74,24 +74,7 @@ def listaparaalteraatendente(pk):
     return render_template('alteraatendente.html', datas=data, pk = pk)
 
 
-# @app.route('/alteraratendente/<int:pk>/', methods=['POST', 'GET'])
-# def alteraratendente(pk):
-#     cpfAtendente = request.form['cpfAtendente']
-#     nomeAtendente = request.form['nomeAtendente']
-#     sobrenomeAtendente = request.form['sobrenomeAtendente']
-#     rgAtendente = request.form['rgAtendente']
-#     enderecoAtendente = request.form['enderecoAtendente']
-#     salarioAtendente = request.form['salarioAtendente']
-#     telefoneAtendente = request.form['telefoneAtendente']
 
-#     if cpfAtendente and nomeAtendente and sobrenomeAtendente and rgAtendente and enderecoAtendente and salarioAtendente and telefoneAtendente:
-#         conn = mysql.connect()
-#         cursor = conn.cursor()
-#         cursor.execute('UPDATE Atendente SET CpfAtendente=%s, NomeAtendente=%s, SobrenomeAtendente=%s, RgAtendente=%s, EnderecoAtendente=%s, SalarioAtendente=%s, TelefoneAtendente=%s WHERE idAtendente=%s',
-#                        (cpfAtendente, nomeAtendente, sobrenomeAtendente, rgAtendente, enderecoAtendente, salarioAtendente, str(pk)))
-#         conn.commit()
-
-#     return render_template('alteraatendente.html', pk = pk)
 
 
 
