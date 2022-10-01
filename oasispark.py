283cce186c5472942975fcc16558e397257d4cb1
rafaelgac98 <80112965+rafaelgac98@users.noreply.github.com>
2022-09-30 21:00:59 -0300

    Update oasispark.py

--- oasispark.py
+++ oasispark.py
@@ -182,54 +182,54 @@ def selectatendente():
 #DELETE
 
 #DELETAR ATENDENTE
-@app.route('/deletaratendente/<int:pk>/', methods=['GET'])
-def deletaratendente(pk):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute('DELETE from Atendente where idAtendente = ' + str(pk))
-    data = cursor.fetchall()
-    conn.commit()
-    return render_template('cadastroatendente.html', datas=data, pk = pk)
+# @app.route('/deletaratendente/<int:pk>/', methods=['GET'])
+# def deletaratendente(pk):
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
+#     cursor.execute('DELETE from Atendente where idAtendente = ' + str(pk))
+#     data = cursor.fetchall()
+#     conn.commit()
+#     return render_template('cadastroatendente.html', datas=data, pk = pk)
 
-#DELETAR MANOBRISTA
-@app.route('/deletarmanobrista/<int:pk>/', methods=['GET'])
-def deletaratendente(pk):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute('DELETE from Manobrista where idManobrista = ' + str(pk))
-    data = cursor.fetchall()
-    conn.commit()
-    return render_template('cadastromanobrista.html', datas=data, pk = pk)
+# #DELETAR MANOBRISTA
+# @app.route('/deletarmanobrista/<int:pk>/', methods=['GET'])
+# def deletaratendente(pk):
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
+#     cursor.execute('DELETE from Manobrista where idManobrista = ' + str(pk))
+#     data = cursor.fetchall()
+#     conn.commit()
+#     return render_template('cadastromanobrista.html', datas=data, pk = pk)
 
-#DELETAR CLIENTE
-@app.route('/deletarcliente/<int:pk>/', methods=['GET'])
-def deletaratendente(pk):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute('DELETE from Cliente where idCliente = ' + str(pk))
-    data = cursor.fetchall()
-    conn.commit()
-    return render_template('cadastrocliente.html', datas=data, pk = pk)
+# #DELETAR CLIENTE
+# @app.route('/deletarcliente/<int:pk>/', methods=['GET'])
+# def deletaratendente(pk):
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
+#     cursor.execute('DELETE from Cliente where idCliente = ' + str(pk))
+#     data = cursor.fetchall()
+#     conn.commit()
+#     return render_template('cadastrocliente.html', datas=data, pk = pk)
 
-#DELETAR VAGA
-@app.route('/deletarvaga/<int:pk>/', methods=['GET'])
-def deletaratendente(pk):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute('DELETE from Vaga where idVaga = ' + str(pk))
-    data = cursor.fetchall()
-    conn.commit()
-    return render_template('cadastrovaga.html', datas=data, pk = pk)
+# #DELETAR VAGA
+# @app.route('/deletarvaga/<int:pk>/', methods=['GET'])
+# def deletaratendente(pk):
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
+#     cursor.execute('DELETE from Vaga where idVaga = ' + str(pk))
+#     data = cursor.fetchall()
+#     conn.commit()
+#     return render_template('cadastrovaga.html', datas=data, pk = pk)
 
-#DELETAR VEICULO
-@app.route('/deletarveiculo/<int:pk>/', methods=['GET'])
-def deletaratendente(pk):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute('DELETE from Veiculo where idVeiculo = ' + str(pk))
-    data = cursor.fetchall()
-    conn.commit()
-    return render_template('cadastroveiculo.html', datas=data, pk = pk)
+# #DELETAR VEICULO
+# @app.route('/deletarveiculo/<int:pk>/', methods=['GET'])
+# def deletaratendente(pk):
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
+#     cursor.execute('DELETE from Veiculo where idVeiculo = ' + str(pk))
+#     data = cursor.fetchall()
+#     conn.commit()
+#     return render_template('cadastroveiculo.html', datas=data, pk = pk)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5005))
